c1077d6c4702b1bdfb1cc9f685d72e84b1091bff
Vannent <98601749+Vannent@users.noreply.github.com>
2022-10-06 12:54:57 -0700

    feat: add apollo and express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,43 @@
-import { MikroORM } from "@mikro-orm/core";
+import "reflect-metadata";
+import { MikroORM, RequestContext } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   orm.getMigrator().up();
-  const emFork = orm.em.fork();
-  // const post = emFork.create(Post, { title: "test post" });
-  // await emFork.persistAndFlush(post);
+  // const emFork = orm.em.fork();
 
-  // const posts = await emFork.find(Post, {});
-  // console.log(posts);
+  const app = express();
+
+  let apolloServer = null;
+  const startServer = async () => {
+    apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [HelloResolver, PostResolver],
+        validate: false,
+      }),
+      context: () => ({ em: orm.em }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+  };
+
+  startServer();
+
+  app.use((_, __, next) => {
+    RequestContext.create(orm.em, next);
+  });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
