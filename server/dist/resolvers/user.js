4913f66befad8a11e9b1bd7080ef603f63b05f8e
Vannent <98601749+Vannent@users.noreply.github.com>
2022-10-10 15:25:03 -0700

    feat: add bcrypt

--- server/dist/resolvers/user.js
+++ server/dist/resolvers/user.js
@@ -8,20 +8,135 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserResolver = void 0;
+const User_1 = require("../entities/User");
 const type_graphql_1 = require("type-graphql");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+let FieldError = class FieldError {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    (0, type_graphql_1.Field)(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], UserResponse);
 let UserResolver = class UserResolver {
-    register() {
-        return "bye";
+    async register(username, password, { em }) {
+        if (username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "Username needs to be longer than two characters.",
+                    },
+                ],
+            };
+        }
+        if (password.length < 8) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Password needs to be at least eight characters long.",
+                    },
+                ],
+            };
+        }
+        const hashedPassword = await bcrypt_1.default.hash(password, 10);
+        const user = em.create(User_1.User, {
+            username,
+            password: hashedPassword,
+        });
+        try {
+            await em.persistAndFlush(user);
+        }
+        catch (err) {
+            if (err.code === "23505" || err.detail.includes("already exists")) {
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "Username is already taken.",
+                        },
+                    ],
+                };
+            }
+        }
+        return { user };
+    }
+    async login(username, password, { em }) {
+        const user = await em.findOne(User_1.User, {
+            username,
+        });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "Could not find that username.",
+                    },
+                ],
+            };
+        }
+        const validPassword = await bcrypt_1.default.compare(password, user.password);
+        if (!validPassword) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Incorrect password.",
+                    },
+                ],
+            };
+        }
+        return {
+            user,
+        };
     }
 };
 __decorate([
-    (0, type_graphql_1.Mutation)(() => String),
+    (0, type_graphql_1.Mutation)(() => UserResponse),
+    __param(0, (0, type_graphql_1.Arg)("username", () => String)),
+    __param(1, (0, type_graphql_1.Arg)("password", () => String)),
+    __param(2, (0, type_graphql_1.Ctx)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", void 0)
+    __metadata("design:paramtypes", [String, String, Object]),
+    __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => UserResponse),
+    __param(0, (0, type_graphql_1.Arg)("username", () => String)),
+    __param(1, (0, type_graphql_1.Arg)("password", () => String)),
+    __param(2, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], UserResolver);
